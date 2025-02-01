6540150922c0e5f42a300691dd4c89e6e7c2027f
G. P. <georgepolimenakos@outlook.com>
2025-02-01 13:22:09 +0000

    update init file with keypoint node mappings

--- __init__.py
+++ __init__.py
@@ -5,13 +5,16 @@ from .image_simhash_nodes import (
     ImageReferenceUpdateNode,
     FrameBlendNode
 )
+from .keypoints_to_img_2d import (KeypointsToPoseNode, KeypointsInputNode)
 
 NODE_CLASS_MAPPINGS = {
     "SimHashCompare": SimHashCompareNode,
     "ImageSelector": ImageSelectorNode,
     "TemporalConsistency": TemporalConsistencyNode, 
     "ImageReferenceUpdate": ImageReferenceUpdateNode,
-    "FrameBlend": FrameBlendNode
+    "FrameBlend": FrameBlendNode,
+    "KeypointsToPose": KeypointsToPoseNode,
+    "KeypointsInput": KeypointsInputNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,5 +22,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageSelector": "Image Selector",
     "TemporalConsistency": "Temporal Consistency",
     "ImageReferenceUpdate": "Update Image Reference",
-    "FrameBlend": "Frame Blend"
+    "FrameBlend": "Frame Blend",
+    "KeypointsToPose": "Keypoints to Pose",
+    "KeypointsInput": "Keypoints Input"
 }
